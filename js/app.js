682377abf2c991f45f4582b217136ebcf3a6db20
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-03 15:21:43 +0100

    FEAT trying to comunicate with array for email (?)

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 console.log("funziono!")
 
 
+//creo una lista di email fittizie con un ARRAY
+
 const arrayMail = ['walterwhite@meth.com','gusfring@cartel.com','jessepinkman@science.com','saulgoodman@law.com']
 console.log(arrayMail)
 
@@ -17,3 +19,33 @@ console.log(mailJesse)
 
 let mailSaul = arrayMail[3]
 console.log(mailSaul)
+
+
+//creo un PROMPT che chiede la mail all' UTENTE
+
+const insertMail = prompt('enter your email pal')
+console.log(insertMail)
+
+let yourMail = insertMail
+console.log(yourMail)
+
+//utilizzare la Mail inserita per CONTROLLARE se e' nella lista dell'ARRAY
+
+let mailCaseNotExisting = []
+console.log(mailCaseNotExisting)
+
+let mailCaseExisting = []
+console.log(mailCaseExisting)
+
+for (i = 0; i < arrayMail.length; i++) {
+
+  if (mailCaseNotExisting !== arrayMail) {
+    mailCaseNotExisting.push('questa mail NON esiste')
+  } else {
+    (mailCaseExisting === arrayMail)
+    mailCaseExisting.push('questa mail esiste!')
+  }
+  
+  console.log(mailCaseNotExisting, mailCaseExisting)
+
+}
