21eb81b974140a3bd289819819bb9f8faca5ae7d
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-03 17:44:25 +0100

    FIX mail true false finding works

--- js/app.js
+++ js/app.js
@@ -22,30 +22,21 @@ console.log(insertMail)
 // il risultato inserito dall'utente nel prompt deve essere CONTROLLATO rispetto alle mail dell'Array e andra' a finire
 // negli ARRAY  in base al RISULTATO
 
-let mailExisting = []
-
-let mailNotExisting = []
+let mailTrovata = false
 
 for (i = 0; i < arrayMail.length; i++) {
   console.log(arrayMail[i])
   if (arrayMail[i] === insertMail) {
-    mailExisting.push("ESISTE!")
-  } else {
-    mailNotExisting.push("NON ESISTE!")
-  }
-  
+    mailTrovata = true
+    console.log(mailTrovata, "ho trovato")
+  } 
 }
-
-console.log(mailExisting, mailNotExisting)
-
-
-
-// scrivere nell'HTML il risultato pushato dal forche va dentro i due Array mailExisting e mailNotExisting
+console.log(mailTrovata)
 
 
 
+//il for qui sopra non ha bisogno di un'ulteriore else con mailtrovata = false, perche gia la condizione iniziale di mailTrovata
+// che posso trovare a riga 25 (fuori dal for), possiede gia' la condizione di false di BASE, quindi riscriverla dentro il FOR
+// creerebbe solamente un bug, ed anche una ripetizione inutile del suo valore gia' di base fuori dal FOR..!!!!!
 
-// document.getElementById("output-esiste").innerHTML = mailExisting[i]
-
-
-
+mailTrovata.doc
