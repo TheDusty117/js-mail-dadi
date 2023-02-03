1039f2709dab580f98be02a28ceecc9e09ecd5f4
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-03 15:02:27 +0100

    FEAT start, try with arrays

---
+++ js/app.js
@@ -0,0 +1,9 @@
+console.log("funziono!")
+
+
+const arrayMail = ['walterwhite@meth.com','gusfring@cartel.com','jessepinkman@science.com','saulgoodman@law.com']
+console.log(arrayMail)
+
+console.log(arrayMail.length)
+
+let mailWalter = arrayMail[0]
