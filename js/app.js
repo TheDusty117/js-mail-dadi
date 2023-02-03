d39362f2a7c0df2e5a560175b94ed858a5672286
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-03 15:04:46 +0100

    FEAT console.log for each mail done and try

--- js/app.js
+++ js/app.js
@@ -7,3 +7,13 @@ console.log(arrayMail)
 console.log(arrayMail.length)
 
 let mailWalter = arrayMail[0]
+console.log(mailWalter)
+
+let mailGus = arrayMail[1]
+console.log(mailGus)
+
+let mailJesse = arrayMail[2]
+console.log(mailJesse)
+
+let mailSaul = arrayMail[3]
+console.log(mailSaul)
